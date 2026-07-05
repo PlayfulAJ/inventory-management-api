e6a0aaf2df2d745b45ed2d996dd5ca9ea79c2221
PlayfulAJ <jabaringure@gmail.com>
2026-07-05 16:29:19 +0300

    Creating the GET all inventory.

--- app.py
+++ app.py
@@ -2,11 +2,35 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-# This is the  Home route
-# it displays a welcome message.
+# Simulated inventory database
+inventory = [
+    {
+        "id": 1,
+        "name": "Organic Almond Milk",
+        "brand": "Almond",
+        "price": 350,
+        "stock": 20
+    },
+    {
+        "id": 2,
+        "name": "Peanut Butter",
+        "brand": "Nutty",
+        "price": 500,
+        "stock": 15
+    }
+]
+
+# This is the Home route.
+# It displays a welcome message.
 @app.route("/")
 def home():
     return jsonify({"message": "Welcome to the Inventory Management API"})
+
+
+# This route returns all inventory items.
+@app.route("/inventory", methods=["GET"])
+def get_inventory():
+    return jsonify(inventory)
 
 
 if __name__ == "__main__":
