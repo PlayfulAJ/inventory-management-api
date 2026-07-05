3a5e1c12a08a0e3df21e5f460100fcb39ed61fcb
PlayfulAJ <jabaringure@gmail.com>
2026-07-05 21:14:50 +0300

    Finallizing The inventory management api wit tests and a README

---
+++ README.md
@@ -0,0 +1,32 @@
+# Inventory Management API
+
+## Overview
+
+The Inventory Management API allows users to manage inventory items. The application connects to the OpenFoodFacts API to retrieve product information using a barcode.
+
+A simple CLI is also included to allow users to interact with the API from the terminal.
+---
+
+## Features
+
+* View all inventory items
+* View a single inventory item by ID
+* Add a new inventory item
+* Update an item's price or stock
+* Delete an inventory item
+* Search products using the OpenFoodFacts API
+* Simple CLI for interacting with the API
+* Unit tests using Pytest
+
+---
+
+## Technologies Used
+
+* Python
+* Flask
+* Requests
+* Pytest
+
+## Author
+
+Jabari Ngure
---
+++ tests/test_app.py
@@ -0,0 +1,66 @@
+from app import app
+
+client = app.test_client()
+
+
+# Testing the home route.
+def test_home():
+    response = client.get("/")
+    assert response.status_code == 200
+
+
+# Testing to getting all inventory items.
+def test_get_inventory():
+    response = client.get("/inventory")
+    assert response.status_code == 200
+
+
+# Testing to getting one inventory item.
+def test_get_inventory_item():
+    response = client.get("/inventory/1")
+    assert response.status_code == 200
+
+
+# Test adding a new inventory item.
+def test_add_inventory_item():
+
+    new_item = {
+        "name": "Milk",
+        "brand": "Brookside",
+        "price": 250,
+        "stock": 10
+    }
+
+    response = client.post("/inventory", json=new_item)
+
+    assert response.status_code == 200
+
+
+# Test updating an inventory item.
+def test_update_inventory_item():
+
+    response = client.patch(
+        "/inventory/1",
+        json={
+            "price": 400,
+            "stock": 30
+        }
+    )
+
+    assert response.status_code == 200
+
+
+# Test deleting an inventory item.
+def test_delete_inventory_item():
+
+    response = client.delete("/inventory/2")
+
+    assert response.status_code == 200
+
+
+# Test searching the OpenFoodFacts API.
+def test_search_product():
+
+    response = client.get("/search/737628064502")
+
+    assert response.status_code == 200
